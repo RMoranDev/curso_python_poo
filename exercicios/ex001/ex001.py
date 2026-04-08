7d2c74c2276727d04da0fc846d744304aeb701ac
RMoranDev <ricardo.moranc2@gmail.com>
2026-04-08 16:17:01 -0300

    Piloto

---
+++ exercicios/ex001/ex001.py
@@ -0,0 +1,29 @@
+# Declaracao de classes
+class Gafanhoto:
+    def __init__(self): # Metodo construtor
+        # Atributos de instancia
+        self.nome = ""
+        self.idade = 0
+
+    # Metodos de instancia
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto e tem {self.idade} anos de idade."
+
+# Declaracao de Objetos
+g1 = Gafanhoto()
+g1.nome = "Maria"
+g1.idade = 17
+g1.aniversario()
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = "Mauro"
+g2.idade = 57
+g2.aniversario()
+print(g2.mensagem())
+
+g3 = Gafanhoto()
+print(g3.mensagem())
